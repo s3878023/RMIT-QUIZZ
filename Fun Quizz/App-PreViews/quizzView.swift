5ba1784e23aaee7822420ebff0afb4ac0260b375
Nguyen Duc Dai <73817832+s3878023@users.noreply.github.com>
2023-09-06 20:05:07 +0700

    Add files via upload

--- Fun Quizz/App-PreViews/quizzView.swift
+++ Fun Quizz/App-PreViews/quizzView.swift
@@ -1,10 +1,26 @@
 //
 //  quizzView.swift
-//  Fun Quizz
-//
-//  Created by Đại Đức on 01/09/2023.
-//
-
+//  Fun Quizz-1
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Nguyen Duc Dai
+  ID: s3878023
+  Created  date: 19/08/2023
+  Last modified: 06/09/2023
+  Acknowledgement:
+  https://opentdb.com/api_config.php
+  https://designcode.io/quick-apps-swiftui-trivia-app-1
+  https://designcode.io/quick-apps-swiftui-trivia-app-2
+  https://designcode.io/quick-apps-swiftui-trivia-app-3
+  https://designcode.io/quick-apps-swiftui-trivia-app-4
+  https://chat.openai.com/
+  https://www.flaticon.com/
+  https://pixabay.com/sound-effects/
+  https://www.epidemicsound.com/sound-effects/?_us=adwords&_usx=11440449526_sound%20effect%20download&utm_source=google&utm_medium=paidsearch&utm_campaign=11440449526&utm_term=sound%20effect%20download&gclid=Cj0KCQjwxuCnBhDLARIsAB-cq1ocpAypZVL6CuM_aquczghuF7qSmqBzmdP0VRUsxGeZ4QPPI3j6D_AaAsEbEALw_wcB
+*/
 import SwiftUI
 import AVFoundation
 struct quizzView: View {
